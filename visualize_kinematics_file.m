d1cdb1cd7a01dc169b87a7525d622ba4fc613f8d
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2015-03-18 15:21:36 +0100

    use evaluate_kinematics_file_time everywhere

--- visualize_kinematics_file.m
+++ visualize_kinematics_file.m
@@ -18,16 +18,7 @@ kine = read_kinematics_file(file);
 
 %% construct angles from Fourier series
 time = 0:1e-3:1;
-
-phi   = zeros(size(time));
-alpha = zeros(size(time));
-theta = zeros(size(time));
-
-for it = 1:length(time)
-   phi(it)   = Fseriesval( [kine.a0_phi; kine.ai_phi], kine.bi_phi, time(it) );
-   alpha(it) = Fseriesval( [kine.a0_alpha; kine.ai_alpha], kine.bi_alpha, time(it) );
-   theta(it) = Fseriesval( [kine.a0_theta; kine.ai_theta], kine.bi_theta, time(it) );
-end
+[phi,alpha,theta] = evaluate_kinematics_file_time(time,kine);
 
 
 
